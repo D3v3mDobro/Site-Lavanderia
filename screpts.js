e6ecd51d41d4ef55d42f2857ee3ed0bc68414944
jjonas_ramos <ramosbobby394@gmail.com>
2025-04-16 11:57:21 -0300

    meu primeiro commit

---
+++ screpts.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body")
+let lavanderia = document.querySelector(".imagem-lavanderia")
+
+function mudarvisual (cor, imagem) {
+    body.style.background = cor
+    lavanderia.src = imagem  }
